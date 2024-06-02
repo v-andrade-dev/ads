7ed019d0f17c315f37d9f12e553f213e37a28ef8
v-andrade-dev <vini.andrade0@gmail.com>
2024-06-02 17:45:27 -0300

    Clonado template material ui, e adicionado paginação a tabela

---
+++ modulo10/template-materialui/src/pages/tarefa/ListarTarefa.jsx
@@ -0,0 +1,288 @@
+import React, { useState, useEffect } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { TableFooter, TablePagination } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Modal from '@mui/material/Modal';
+
+
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { useTheme } from '@mui/material/styles';
+
+
+import CriarTarefa from './CriarTarefa';
+import EditarTarefa from './EditarTarefa';
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, float: 'inline-end'}}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+//A função abaixo é usada para criar o array contendo os dados iniciais da listagem de tarefas.
+function createData(
+  idTarefa,
+  tituloTarefa,
+  descricaoTarefa,
+  inicioTarefa,
+  fimTarefa,
+  statusTarefa,
+  recursoTarefa,
+) {
+  return { idTarefa, tituloTarefa, descricaoTarefa, inicioTarefa, fimTarefa, statusTarefa, recursoTarefa };
+}
+
+//Definição do array contendo os dados iniciais da listagem de tarefas
+const initialRows = [
+  createData(1, 'Tarefa 1', 'Descrição da Tarefa 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
+  createData(2, 'Tarefa 2', 'Descrição da Tarefa 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
+  createData(3, 'Tarefa 3', 'Descrição da Tarefa 3', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
+  createData(4, 'Tarefa 4', 'Descrição da Tarefa 4', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
+  createData(5, 'Tarefa 5', 'Descrição da Tarefa 5', '2022-01-06', '2022-01-07', 'Em Andamento', 'Recurso 5'),
+  createData(6, 'Tarefa 6', 'Descrição da Tarefa 6', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
+];
+
+//Componente ListarTarefa
+const ListarTarefa = () => {
+  const [open, setOpen] = useState(false);
+  const [openEditar, setOpenEditar] = useState(false);
+  const [tarefas, setTarefas] = useState([]);
+  const [tarefa, setTarefa] = useState();
+  const [idTarefaSelecionada, setIdTarefaSelecionada] = useState([]);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleOpenEditar = () => setOpenEditar(true);
+  const handleCloseEditar = () => setOpenEditar(false);
+
+  //O array definido acima é setado como conteúdo do state Tarefas na renderização inicial do componente.
+  useEffect(() => {
+    setTarefas(initialRows);
+  },[]);
+
+  const handleEditar = (id) => {
+    setIdTarefaSelecionada(id);
+
+    //Objeto local para armazenamento da tarefa filtrada de acordo com a seleção do usuário
+    let tarefaParaEditar = tarefas.filter(obj => {
+      return obj.idTarefa === id;
+    })[0];
+
+    //Atribuição do Objeto local, setado acima, ao state Tarefa
+    setTarefa(tarefaParaEditar);
+
+    //Seta como true o state responsável pela exibição do Model de Editar Tarefa
+    setOpenEditar(true)
+  };
+
+  const handleDeletar = (id) => {
+    setTarefas(current =>
+      current.filter(tarefa => {
+        return tarefa.idTarefa !== id;
+      }),
+    );
+  };
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(2);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - tarefas.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+    return(
+    <>
+    <Card>
+        <CardHeader
+          title="Tarefas"
+          subheader="Listagem de Tarefas"
+        /> 
+        <CardContent>
+            <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>#</TableCell>
+                    <TableCell>Título</TableCell>
+                    <TableCell align="right">Descrição</TableCell>
+                    <TableCell align="right">Data de Início</TableCell>
+                    <TableCell align="right">Data de Finalização</TableCell>
+                    <TableCell align="right">Status</TableCell>
+                    <TableCell align="right">Recurso</TableCell>
+                    <TableCell align="left"></TableCell>
+                    <TableCell align="left"></TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {(rowsPerPage > 0
+            ? tarefas.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : tarefas
+          ).map((row, indice) => (
+                    <TableRow
+                    key={indice}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                          {row.idTarefa}
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                          {row.tituloTarefa}
+                      </TableCell>
+                      <TableCell align="right">{row.descricaoTarefa}</TableCell>
+                      <TableCell align="right">{row.inicioTarefa}</TableCell>
+                      <TableCell align="right">{row.fimTarefa}</TableCell>
+                      <TableCell align="right">{row.statusTarefa}</TableCell>
+                      <TableCell align="right">{row.recursoTarefa}</TableCell>
+                      <TableCell align="center">
+                        <Button variant="contained" color="success" onClick={() => handleEditar(row.idTarefa)}><EditIcon fontSize="small" /></Button>            
+                      </TableCell>
+                      <TableCell align="center">
+                        <Button variant="contained" color="error" onClick={() => handleDeletar(row.idTarefa)}><DeleteIcon fontSize="small" /></Button>            
+                      </TableCell>
+                    </TableRow>
+                ))}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+                </TableBody>
+          <TableFooter>
+          <TableRow>
+            <TablePagination 
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={tarefas.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  inputProps: {
+                    'aria-label': 'rows per page',
+                  },
+                  native: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+            </Table>
+            </TableContainer>
+        </CardContent>
+        <CardActions>
+            <Button size="small" variant="contained" onClick={handleOpen}>Criar Tarefa</Button>
+            <Button size="small" variant="outlined">Cancelar</Button>
+      </CardActions> 
+    </Card>
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+          <CriarTarefa handleClose={handleClose} tarefas={tarefas} setTarefas={setTarefas} />
+        </div>
+      </Modal>  
+    </div>
+    <div>
+      <Modal
+        open={openEditar}
+        onClose={handleCloseEditar}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+          <EditarTarefa handleCloseEditar={handleCloseEditar} idTarefaSelecionada={idTarefaSelecionada} tarefas={tarefas} tarefa={tarefa} setTarefas={setTarefas} />
+        </div>
+      </Modal>  
+    </div>
+  </>    
+ );
+};
+
+
+export default ListarTarefa;
